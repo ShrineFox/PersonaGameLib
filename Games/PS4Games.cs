551134f505d86765914f84e607d3212e64265f3e
ShrineFox <ShrineFox@users.noreply.github.com>
2023-02-27 19:38:54 -0500

    Add EUR P3D/P5D PS4 metadata

--- Games/PS4Games.cs
+++ Games/PS4Games.cs
@@ -35,15 +35,25 @@ namespace PersonaGameLib
                 Patches = Patches.PS4P3DUSAPatches,
                 UpdatePKGMinSize = 13631488, UpdatePKGName = "UP2611-CUSA12636_00-PERSONA3DUS00000-A0101-V0100.pkg",
                 ImageUrl = "https://cdn.mobygames.com/covers/7364420-persona-3-dancing-in-moonlight-front-cover.jpg" },
+            new Game() { Name = "Persona 3 Dancing", ShortName = "P3D", TitleID = "CUSA12810", Region = "EUR",
+                Patches = Patches.PS4P3DUSAPatches,
+                UpdatePKGMinSize = 13631488, UpdatePKGName = "EP2475-CUSA12810_00-PERSONA3DEU00000-A0101-V0100.pkg",
+                ImageUrl = "https://cdn.mobygames.com/covers/7364420-persona-3-dancing-in-moonlight-front-cover.jpg" },
+
             new Game() { Name = "Persona 4 Dancing", ShortName = "P4D", TitleID = "CUSA12811", Region = "EUR",
                 CRC32 = "9B2CDA6E", MD5 = "7794E4C921AFB7B759F481C564AFF1CB", SHA1 = "176AF1377B97217E2E02DCC855E64FB730B80960",
                 Patches = Patches.PS4P4DEURPatches,
                 UpdatePKGMinSize = 11993088, UpdatePKGName = "EP2475-CUSA12811_00-PERSONA4DEU00000-A0101-V0100.pkg",
                 ImageUrl = "https://cdn.mobygames.com/covers/2024487-persona-4-dancing-all-night-front-cover.jpg" },
+
             new Game() { Name = "Persona 5 Dancing", ShortName = "P5D", TitleID = "CUSA12380", Region = "USA",
                 CRC32 = "50A4FB33", MD5 = "D4E08AE0C5C7027B4FC01E55CDD23EF6", SHA1 = "521C247ADB8AA675C370257D53D60B77812E343A",
                 Patches = Patches.PS4P5DUSAPatches,
                 UpdatePKGMinSize = 13631488, UpdatePKGName = "UP2611-CUSA12380_00-PERSONA5DUS00000-A0101-V0100.pkg",
+                ImageUrl = "https://cdn.mobygames.com/covers/7364423-persona-5-dancing-in-starlight-front-cover.jpg" },
+            new Game() { Name = "Persona 5 Dancing", ShortName = "P5D", TitleID = "CUSA12813", Region = "EUR",
+                Patches = Patches.PS4P5DUSAPatches,
+                UpdatePKGMinSize = 13631488, UpdatePKGName = "EP2475-CUSA12813_00-PERSONA5DEU00000-A0101-V0100.pkg",
                 ImageUrl = "https://cdn.mobygames.com/covers/7364423-persona-5-dancing-in-starlight-front-cover.jpg" },
         };
     }
